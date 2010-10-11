3ae1aae9ec8997c74d39fb448d22e1512dd285da
Simon Lenz <lenz.simon@googlemail.com>
2010-10-11 23:58:48 +0200

    initial commit

---
+++ src/main.py
@@ -0,0 +1,8 @@
+'''
+Created on 11.10.2010
+
+@author: simon
+'''
+
+if __name__ == '__main__':
+    pass
---
+++ src/util/constants.py
@@ -0,0 +1,11 @@
+'''
+Created on 11.10.2010
+
+@author: simon
+'''
+
+# TODO: move constants to a options class or something
+RESOLUTION = (800, 480)
+ISSOUND = False
+ISDEBUG = False
+ISFULLSCR = False
